418456610c58b6e2bda7a730e4a32f4a8555dfdd
ElBastardoDeKristian <jiglesias@alumno.huergo.edu.ar>
2023-05-28 19:33:23 -0300

    Agregue cosas al practico de ssh, pero falta corroborar todo

--- main.py
+++ main.py
@@ -14,5 +14,78 @@ def main():
     print(stdout.read().decode())
     ssh_client.close()
 
+def execute_command(client, command):
+
+    channel = client.get_transport().open_session()
+
+    channel.exec_command(command)
+
+    output = channel.recv(1024).decode('utf-8')
+    return output
+
+def main2():
+
+    print("Estaremos implementando el ejercicio de bandit3 --> bandit4")
+    print(linea())
+    print("La consigna es la siguiente:")
+    print("- La contraseña del siguiente nivel esta almazenada en una archivo oculto dentro del directorio 'inhere'")
+    print("Para esto haremos la conexion atraves de la libreria paramiko de python")
+    print("Nos conectaremos atraves del protocolo SSH, para esto requerimos el nombre del host, el puerto y el usuario, ademas de la contraseña")
+    print(linea())
+    
+    client = paramiko.SSHClient()
+
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    host_name = "bandit.labs.overthewire.org"
+    port = 2220
+    user_name = "bandit3"
+    password = "aBZ0W5EmUfAf7kHTQeOwd8bauFJ2lAiG"
+
+    client.connect(host_name, port=port, username=user_name, password=password)
+
+    print(linea())
+    print("Tras haber conectado, seguiremos las instrucciones")
+    print("Veremos si existe el directorio inhere")
+
+    print(linea())
+    print(execute_command(client, 'ls'))
+    print(linea())
+
+    print("Luego iremos a ese directorio")
+    execute_command(client, 'cd inhere')
+    print(linea())
+
+    print("Tras esto, veremos si hay algun archivo entro del directorio:")
+    print("Ejecutaremos ls")
+    print(linea())
+    print(execute_command(client, 'ls'))
+    print(linea())
+
+    print("Como vemos, no aparece nada, eso es por que el archivo esta oculo, eso quiere decir que tendremos que hacerle especificaciones a ls")
+    print("Usaremos el comando 'ls -a', la a siendo de absoluto, lo cual nos permitira acceder a los archivos ocultos")
+    print(linea())
+
+    print(execute_command(client, 'ls -a'))
+    print(linea())
+
+    print("Como vemos, hay un archivo que se llama .hidden, accedderemos a ese archivo con el comando cat .hidden")
+    print(linea())
+    print(execute_command(client, 'cat .hidden'))
+    print(linea())
+
+    print("Asi obtendremos la contraseña para el siguiente nivel")
+
+
+
+    
+
+def linea(n=1):
+    result = ""
+    for i in range(n):
+        result+= "------------------------\n"
+    return result
+
+
 if __name__=='__main__':
-    main()
+    main2()
