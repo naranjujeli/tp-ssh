bc91417c7775d40c078f081be9d8a76781f1f873
ruohl <rudolfgbohl@hotmail.com>
2023-05-29 17:58:51 -0300

    Anda bien

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import paramiko
 
 def main():
-    command = "echo Hello World!"
-    host = "16.16.8.216"
-    username = "bandit0"
-    password = "bandit0"
-    port = 2220
+    bold = "\033[1m"
+    white = "\033[0m"
 
-    ssh_client = paramiko.client.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(host, username=username, password=password, port=port)
-    stdin, stdout, stderr = ssh_client.exec_command(command)
-    print(stdout.read().decode())
-    ssh_client.close()
+    print(bold + "HOlaaaaaaaaa" + white)
+
+    
 
 def execute_command(client, command):
 
@@ -23,16 +17,28 @@ def execute_command(client, command):
     output = channel.recv(1024).decode('utf-8')
     return output
 
+def clear_prompt():
+    
+    import os
+    if os.environ == "cmd":
+        os.system('cls')
+    else:
+        os.system('clear')
+        
+def wait_clear():
+    wait()
+    clear_prompt()
+
+def wait():
+    input(":")
+
+def bold(string):
+    bold = "\033[1m"
+    white = "\033[0m"
+    return bold + string + white
+
 def main2():
 
-    print("Estaremos implementando el ejercicio de bandit3 --> bandit4")
-    print(linea())
-    print("La consigna es la siguiente:")
-    print("- La contraseña del siguiente nivel esta almacenada en una archivo oculto dentro del directorio 'inhere'")
-    print("Para esto haremos la conexion atraves de la libreria paramiko de python")
-    print("Nos conectaremos atraves del protocolo SSH, para esto requerimos el nombre del host, el puerto y el usuario, ademas de la contraseña")
-    print(linea())
-    
     client = paramiko.SSHClient()
 
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -44,45 +50,62 @@ def main2():
 
     client.connect(host_name, port=port, username=user_name, password=password)
 
+    clear_prompt()
+
+    print(f"Estaremos implementando el ejercicio de {bold('bandit3 --> bandit4')}")
     print(linea())
-    print("Tras haber conectado, seguiremos las instrucciones")
-    print("Veremos si existe el directorio inhere")
+    print("La consigna es la siguiente:")
+    print(f"- La contraseña del siguiente nivel esta almacenada en un archivo oculto dentro del directorio {bold('inhere')}.")
+    print("Para esto haremos la conexion a traves de la libreria 'paramiko' de python.")
+    print("Nos conectaremos a traves del protocolo SSH, para esto requerimos el nombre del host, el puerto y el usuario, ademas de la contraseña.")
+    print(linea())
+
+    wait_clear()
+
+    print("Tras habernos conectado, seguiremos las instrucciones.")
+    print(f"Veremos si existe el directorio 'inhere', con el comando {bold('ls')}.")
 
     print(linea())
+    print("Output:")
     print(execute_command(client, 'ls'))
     print(linea())
 
-    print("Luego iremos a ese directorio")
+    wait_clear()
+
+    print(f"Luego iremos a ese directorio con el comando {bold('cd inhere')}.")
     execute_command(client, 'cd inhere')
     print(linea())
 
+    wait_clear()
+
     print("Tras esto, veremos si hay algun archivo dentro del directorio:")
-    print("Ejecutaremos ls")
+    print(f"Ejecutaremos {bold('ls')}.")
     print(linea())
-    print(execute_command(client, 'ls'))
-    print(linea())
-
-    print("Como vemos, no aparece nada, eso es por que el archivo esta oculto, eso quiere decir que tendremos que hacerle especificaciones a ls")
-    print("Usaremos el comando 'ls -a', la a siendo de absoluto, lo cual nos permitira acceder a los archivos ocultos")
+    print(execute_command(client, 'cd inhere | ls'))
     print(linea())
 
-    print(execute_command(client, 'ls -a'))
+    wait_clear()
+
+    print("Como vemos, no aparece nada, eso es por que el archivo esta oculto, eso quiere decir que tendremos que hacerle especificaciones a ls.")
+    print(f"Usaremos el comando {bold('ls -a')}, la a siendo de absoluto, lo cual nos permitira acceder a los archivos ocultos.")
+    print(linea())
+    print(execute_command(client, 'cd inhere | ls -a'))
     print(linea())
 
-    print("Como vemos, hay un archivo que se llama .hidden, accederemos a ese archivo con el comando cat .hidden")
+    wait_clear()
+
+    print(f"Como vemos, hay un archivo que se llama '.hidden', accederemos a ese archivo con el comando {bold('cat .hidden')}.")
     print(linea())
-    print(execute_command(client, 'cat .hidden'))
+    print(execute_command(client, 'cd inhere; cat .hidden'))
     print(linea())
 
-    print("Asi obtendremos la contraseña para el siguiente nivel")
+    print("Asi obtendremos la contraseña para el siguiente nivel.")
+    wait_clear()
 
-
-
-    
 
 def linea(n=1):
-    result = ""
-    for i in range(n):
+    result = "------------------------"
+    for i in range(n-1):
         result+= "------------------------\n"
     return result
 
